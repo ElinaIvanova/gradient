8cbdbc4083c09800ad1b2230d6c4f5e6147e1874
elina <eelinaivanova@gmail.com>
2018-02-27 22:55:20 +0300

    ban portrait orientation in favour of lanscape, gradient on a fixed screen

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 var button = document.querySelector('#button');
+var main = document.querySelector('#main');
 
 function onHover(event) {
   var x = event.pageX - button.offsetLeft - button.offsetParent.offsetLeft;
   var y = event.pageY - button.offsetTop - button.offsetParent.offsetTop;
   button.style.setProperty('--x', x + 'px');
   button.style.setProperty('--y', y + 'px');
-  console.log('hello there');
 }
 
-function onOrientationChange() {
-  console.log(screen.orientation);
+function match() {
+  var portrait = window.matchMedia("(orientation: portrait)");
+  var width = window.innerWidth;
+
+  if(portrait.matches && width < 700) {  
+    // Portrait orientation
+    main.classList.remove('landscape-orientation');
+    main.classList.add('portrait-orientation');
+  } else if (!(portrait.matches) && width < 700) {  
+    // Landscape orientation
+    main.classList.remove('portrait-orientation');
+    main.classList.add('landscape-orientation');
+  } else {
+    main.classList.remove('portrait-orientation');
+    main.classList.remove('landscape-orientation');
+  }
 }
 
 function onResize() {
-  console.log(screen.width, screen.height);
+  match();
 }
 
 function init() {
   button.addEventListener('mousemove', onHover);
-
+  match();
   window.addEventListener('resize', onResize);
-  window.addEventListener('orientationchange', onOrientationChange);
 }
 
 init();
